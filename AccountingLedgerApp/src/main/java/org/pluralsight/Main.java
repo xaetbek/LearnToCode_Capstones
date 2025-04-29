9d5673e865f248b26e9c7f562f63920dfb9de599
Hayat A <xaetbek@gmail.com>
2025-04-29 16:29:50 -0700

    Changed the home screen layout

--- AccountingLedgerApp/src/main/java/org/pluralsight/Main.java
+++ AccountingLedgerApp/src/main/java/org/pluralsight/Main.java
@@ -5,30 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("\nWelcome to Accounting Ledger App");
 
-        while (true) {
-            System.out.println("\nD - Add Deposit");
-            System.out.println("P - Make Payment (Debit)");
-            System.out.println("L - Ledger");
-            System.out.println("X - Exit");
-            System.out.print("Enter command: ");
-            String input = scanner.nextLine();
+        TransactionHandler ledgerApp = new TransactionHandler();
 
-            if (input.equalsIgnoreCase("D")) {
-                System.out.println("Add deposit coming soon");
-            } else if (input.equalsIgnoreCase("P")) {
-                System.out.println("Make payment function coming soon");
-            } else if (input.equalsIgnoreCase("L")) {
-                System.out.println("Ledger coming soon ");
-            }else if (input.equalsIgnoreCase("X")) {
-                System.out.println("Goodbye!");
-                return;
-            } else {
-                System.out.println("Invalid command. Please choose D, P, L or X.");
-            }
-        }
-
+        ledgerApp.run();
     }
 }
--- AccountingLedgerApp/src/main/java/org/pluralsight/TransactionHandler.java
+++ AccountingLedgerApp/src/main/java/org/pluralsight/TransactionHandler.java
@@ -1,15 +1,45 @@
 package org.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TransactionHandler {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+        loadTransactions();
+
+        boolean running = true;
+        while (running) {
+            System.out.print("=== Home Screen ===" +
+                    "\nD) Add Deposit" +
+                    "\nP) Make Payment (Debit)" +
+                    "\nL) Ledger" +
+                    "\nX) Exit" +
+                    "\nChoose an option: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("D")) {
+                addDeposit();
+            } else if (input.equalsIgnoreCase("P")) {
+                System.out.println("Make payment function coming soon");
+            } else if (input.equalsIgnoreCase("L")) {
+                System.out.println("Ledger coming soon ");
+            }else if (input.equalsIgnoreCase("X")) {
+                System.out.println("Goodbye!");
+                return;
+            } else {
+                System.out.println("Invalid command. Please choose D, P, L or X.\n");
+            }
+        }
+    }
+
+
     public static List<Transaction> loadTransactions() {
         List<Transaction> transactions = new ArrayList<>();
         File file = new File("transactions.csv");
@@ -42,12 +72,55 @@ public class TransactionHandler {
         } catch (IOException e) {
             throw new RuntimeException("Error reading transactions.csv", e);
         }
-
         return transactions;
     }
 
-    // Save a single transaction to the file
-    public static void writeTransaction() {
+    // Save a single transaction to the CSV file
+    public static void writeTransactionToCsv(String vendor, String description, double amount) {
+        LocalDate transactionDate = LocalDate.now();
+        LocalTime transactionTime = LocalTime.now();
+        try {
+            FileWriter fileWriter = new FileWriter("transactions.csv" , true);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            File file = new File("transactions.csv");
+            // Check if the file is empty
+            if (!file.exists() || file.length() == 0) {
+                // Write the header if the file is empty
+                writer.write("Date|Time|Description|Vendor|Amount\n");
+            }
+            // Write the transaction details
+            String formattedDate = transactionDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String formattedTime = transactionTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            writer.append(formattedDate).append("|").append(formattedTime).append("|").append(description).append("|").append(vendor).append("|").append(String.valueOf(amount)).append("\n");
+            writer.close();
+
+            if (amount > 0) {
+                System.out.println("Deposit: $" + amount + " | Vendor: " + vendor + " | Description: " + "'" + description + "'" + " | Status: Added\n");
+            } else {
+                System.out.println("Payment: $" + Math.abs(amount) + " | Vendor: " + vendor + " | Description: "  + "'" + description + "'" + " | Status: Payment Made\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing transactions to CSV: " + e.getMessage());
+        }
+    }
+
+    public static void addDeposit() {
+        System.out.println("Enter the amount to deposit:");
+        System.out.print("> ");
+        double amount = scanner.nextDouble();
+        if (amount <= 0) {
+            System.out.println("Deposit amount must be greater than zero.");
+            return;
+        }
+        scanner.nextLine();
+        System.out.println("Enter a description for the deposit:");
+        System.out.print("> ");
+        String description = scanner.nextLine();
+        System.out.println("Enter the vendor name for the deposit:");
+        System.out.print("> ");
+        String vendor = scanner.nextLine();
+
+        writeTransactionToCsv(vendor, description, amount);
 
     }
 }
