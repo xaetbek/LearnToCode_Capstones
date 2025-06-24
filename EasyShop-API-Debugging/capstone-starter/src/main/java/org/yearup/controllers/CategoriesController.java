9974b924c01c3d1b5cf330eadfcd0f402cb3d05d
Hayat A <xaetbek@gmail.com>
2025-06-24 15:02:17 -0700

    Update CategoriesController.java
    Added all necessary annotations and created an Autowired controller to inject the categoryDao and ProductDao

--- EasyShop-API-Debugging/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ EasyShop-API-Debugging/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,7 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -12,15 +11,23 @@ import java.util.List;
 
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
+// http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-
     // create an Autowired controller to inject the categoryDao and ProductDao
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao){
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
 
     // add the appropriate annotation for a get action
     public List<Category> getAll()
