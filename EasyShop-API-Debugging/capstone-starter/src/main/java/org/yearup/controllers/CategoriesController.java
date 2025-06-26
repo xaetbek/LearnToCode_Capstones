b9243817ef2f1efcb7786e8f4127b953a4f0221c
Hayat A <xaetbek@gmail.com>
2025-06-26 11:17:16 -0700

    Fixing a bug
    Get category by id is returning wrong https status

--- EasyShop-API-Debugging/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ EasyShop-API-Debugging/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -115,7 +115,7 @@ public class CategoriesController
             // delete the category by id
             categoryDao.delete(id);
         } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Doesn't exist.");
         }
 
     }
